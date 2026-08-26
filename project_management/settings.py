7dcff0f6bcfa1bdd6f0f9c102bde21f6b54b6527
Soumya <nagure7019795023@gmail.com>
2026-08-26 17:37:29 +0530

    Configure PostgreSQL for deployment

--- project_management/settings.py
+++ project_management/settings.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
-"""
 
+"""
 import os
+import dj_database_url
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,11 +91,11 @@ WSGI_APPLICATION = 'project_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
